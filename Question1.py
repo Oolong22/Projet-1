e1ed074a344a4131ce0d0e062eb4bc2dc5880359
Oolong22 <tristansamson21@hotmail.com>
2023-09-18 12:35:21 -0400

    First commit

---
+++ Question1.py
@@ -0,0 +1,13 @@
+# Définition d'une fonction nommée "estNombreDansIntervalle" qui prend trois paramètres :
+    # - min_val : la valeur minimale de l'intervalle
+    # - max_val : la valeur maximale de l'intervalle
+    # - nombre : le nombre à vérifier s'il est dans l'intervalle
+
+def estNombreDansIntervalle(min_val, max_val, nombre):
+    # Vérifier si "min_val" est inférieur ou égal à "nombre"
+    # et si "nombre" est inférieur ou égal à "max_val".
+    # Cela permet de vérifier si "nombre" se trouve dans l'intervalle [min_val, max_val].
+
+    nombreDansIntervalle = 
+
+    return nombreDansIntervalle
